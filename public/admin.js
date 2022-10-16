387cac27c68e5a92ab284637eadf98ed08d4d942
Veny Cordova <venycordova@gmail.com>
2022-10-16 12:03:19 -0400

    added buttons to save new values for each book

--- public/admin.js
+++ public/admin.js
@@ -1,6 +1,6 @@
 
 // Your Code Here
-async function getBooks() {
+async function main() {
     //created fetch function to get books
     let response = await fetch('http://localhost:3001/listBooks')
     let books = await response.json();
@@ -13,22 +13,35 @@ function renderBook(book) {
     //created a list to get book titles
     let newList = document.createElement('li');
     newList.textContent = book.title
-    root.appendChild(newList);
 
     //created a input field to update the book quantity
-    let input = document.createElement('input');
-    input.type = 'text';
-    input.value = book.quantity;
-    root.appendChild(input);
+    let bookInput = document.createElement('input');
+    //input.type = 'text';
+    bookInput.value = book.quantity;
 
-    
+    //create a button to save and add the new inputs
+    let button = document.createElement('button');
+    button.textContent = 'Save';
 
+    //Create a fetch request to change the book quantity
+    button.addEventListener('click', function () {
+        fetch('http://localhost:3001/updateBook', {
+            method:"PATCH",
+            headers:{
+                "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            id: book.id,
+            quantity: bookInput.value,
+        })
+    })
+});
 
+    newList.append(bookInput, button);
+    root.append(newList)
 }
 
-
-
-
+main()
 
 
 
